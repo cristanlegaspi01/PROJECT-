151776be232ba46a1aee03f9b5b684d12cc620a4
cristanlegaspi <cristanlegaspi01@gmai.com>
2024-11-19 22:27:57 +0800

    update admin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import Dashboard from './pages/Main/Dashboard/Dashboard';
+import Main from './pages/Main/Main';
+import Movie from './pages/Main/Movie/Movie';
+import Lists from './pages/Main/Movie/Lists/Lists';
+import Form from './pages/Main/Movie/Form/Form';
+import Login from './pages/Public/Login/Login';
+import Register from './pages/Public/Register/Register';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+
+  {
+    path: '/Register', 
+    element: <Register />,
+  },
+  {
+    path: '/main',
+    element: <Main />,
+    children: [
+      //Temporarily disabled the dashboard route
+      // {
+      //   path: '/main/dashboard',
+      //   element: <Dashboard />,
+      // },
+      {
+        path: '/main/movies',
+        element: <Movie />,
+        children: [
+          {
+            path: '/main/movies',
+            element: <Lists />,
+          },
+          {
+            path: '/main/movies/form/:movieId?',
+            element: <Form />,
+            children: [
+              {
+                path: '/main/movies/form/:movieId',
+                element: (
+                  <h1>Change this for cast & crew CRUD functionality.</h1>
+                ),
+              },
+              {
+                path: '/main/movies/form/:movieId/cast-and-crews',
+                element: (
+                  <h1>
+                    Change this for cast & crew CRUD functionality component.
+                  </h1>
+                ),
+              },
+              {
+                path: '/main/movies/form/:movieId/photos',
+                element: (
+                  <h1>Change this for photos CRUD functionality component.</h1>
+                ),
+              },
+              {
+                path: '/main/movies/form/:movieId/videos',
+                element: (
+                  <h1>Change this for videos CRUD functionality component.</h1>
+                ),
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <RouterProvider router={router} />
     </div>
   );
 }
