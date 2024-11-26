ca64eba93ca9c8f037a2db1e28d25b31f23e6739
Cristan Irish Legaspi <cristanlegaspi01@gmail.com>
2024-11-27 00:01:52 +0800

    Add files via upload

---
+++ admin-dashboard-cristanirish-legaspi-bsit3a/client-movieproject/src/pages/NotFound.js
@@ -0,0 +1,14 @@
+// src/pages/NotFound.js
+
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
